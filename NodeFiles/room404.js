d8921068c9b59e5eff3438762eaa994b8377311c
kenziesl <31229103+kenziesl@users.noreply.github.com>
2019-04-26 20:22:58 -0500

    Update room404.js
    Testing GUI still

--- NodeFiles/room404.js
+++ NodeFiles/room404.js
@@ -72,7 +72,7 @@ app.post('/user/login', (req, res) => {
                     req.session.uid = results[0].id;
                     req.session.email = req.body.email;
                     req.session.loggedin = true;
-                    res.status(200).send(req.session.uid);
+                    res.status(200).send(req.session.id);
                 } else { //if log in unsuccessful
                     req.status(400).send('Incorrect Username and/or Password!');
                     req.session.loggedin = false;
@@ -163,7 +163,7 @@ app.get('/user/profile', (req, res) => {
             if (err) throw err;
             else {
 console.log("Session ID is ", req.session.id);
-                res.status(200).send(req.session.uid);
+                res.status(200).send(req.session.id);
             }
         });
    // }
