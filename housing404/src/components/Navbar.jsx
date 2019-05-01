06f92f3f98edf708720e60b57482a1c68c1018b5
caharper <caharper23@gmail.com>
2019-04-30 19:07:41 -0500

    added logo to route back to main in navbar

--- housing404/src/components/Navbar.jsx
+++ housing404/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React , { Component } from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import { AccountRepository } from './../api/accountRepository'
+import { AccountRepository } from './../api/accountRepository';
+import logo from './../housing404.svg';
 
 import './navbar.css'
 
@@ -26,6 +27,24 @@ class Navbar extends Component {
 
     if (!this.state.user) {
       return (<></>)
+    }
+
+    if(!this.props.didChange){
+      return (
+        <nav className="navbar navbar-dark bg-dark justify-content-between postion-relative" >
+
+            <div className="navbar-brand">
+              <Link to="/main">
+                <img className="icon col-2 pl-1 postion-absolute float-left" src={logo}></img>
+              </Link>
+            </div>
+
+            <button onClick={() => this.Logout()} className="logoutButton" >
+                Logout
+                </button>
+
+        </nav>
+      );
     }
 
     return (
